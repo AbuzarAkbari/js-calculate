dd13156367030fbf449800944fa07c72a0a2ba20
abuzar-4a6c <abuzar_voetbal@msn.com>
2017-06-13 18:40:37 +0200

    keer werkt maar ik moet nog aan minus en delen werken

--- js-calculate.js
+++ js-calculate.js
@@ -1,26 +1,42 @@
-//function Optellen(){
-//    document.getElementById("add").innerHTML = var cijfers=[];
-//    var text = "";
-//    var i;
-//    for (i = 0; i < cijfers.length; i++) {
-//        text += cijfers[i] + "<br>";
-//    }
-//    document.getElementById("result").innerHTML = text;
-//}
-
-//    cijfers.push(document.getElementById("numbers").innerHTML);
-//    var symbol = document.getElementById("operation").innerHTML;
-
-
-function Optellen() {
+function bereken() {
+    var operation = document.getElementById("operation").value;
     var cijfers = document.getElementById("numbers").value;
     var apart = cijfers.split(",");
     parseInt(apart);
-    
-    var begin = 0;
+
+    var min = 0;
     for (i = 0; i < apart.length; i++) {
-      begin+=parseFloat(apart[i]);
+        min+=parseFloat(apart[i]);
     }
 
-    document.getElementById("result").innerHTML = begin;
-}
+    var plus = 0;
+
+    var keer = 1;
+    
+    
+    
+    
+    if (operation == "sum") {
+        for (i = 0; i < apart.length; i++) {
+            plus+=parseFloat(apart[i]);
+        }
+        document.getElementById("result").innerHTML = plus;
+    } else if (operation == "substract") {
+        for (i = 0; i < apart.length; i++) {
+            min-=parseFloat(apart[i]);
+        }
+        document.getElementById("result").innerHTML = min;
+    } else if (operation == "multiply") {
+        for (i = 0; i < apart.length; i++) {
+            keer*=parseFloat(apart[i]);
+        }
+        document.getElementById("result").innerHTML = keer;
+    } else if (operation == "divide") {
+        for (i = 0; i < apart.length; i++) {
+            begin/=parseFloat(apart[i]);
+        }
+        	document.getElementById("result").innerHTML = begin;
+    }
+    console.log(begin);
+
+    }
